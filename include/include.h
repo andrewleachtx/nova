53f1e4ba33a68a25529f57da811912801ee36321
andrewleachtx <andrewleach38@gmail.com>
2025-02-13 12:24:31 -0600

    Add libraries and resolve vcpkg conflicts, create README for installation, add toy data, ImGui, GLSL, and other graphics dependencies - working on getting window set up next

---
+++ include/include.h
@@ -0,0 +1,40 @@
+// #pragma once
+// #ifndef INCLUDE_H
+// #define INCLUDE_H
+
+// // Constants & STL
+// #include <cassert>
+// #include <cstring>
+// #define _USE_MATH_DEFINES
+// #include <cmath>
+// #include <iostream>
+// #include <vector>
+// #include <random>
+
+// // GLEW / GLFW
+// #define GLEW_STATIC
+// #include <GL/glew.h>
+// #include <GLFW/glfw3.h>
+
+// // GLM //
+// #define GLM_FORCE_RADIANS
+// #include <glm/glm.hpp>
+// #include <glm/gtc/type_ptr.hpp>
+
+// // Dear ImGui //
+// #include "imgui/imgui.h"
+// #include "imgui/imgui_impl_glfw.h"
+// #include "imgui/imgui_impl_opengl3.h"
+
+// // CUSTOM CLASSES //
+// #include "GLSL.h"
+// #include "FreeCam.h"
+// #include "MatrixStack.h"
+// #include "Program.h"
+// #include "Mesh.h"
+
+// // UTILS //
+// #include "include/utils.h"
+// #include "constants.h"
+
+// #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,26 @@
-#include <iostream>
+// TODO: fix
+// #include "include.h"
+
 #include <dv-processing/core/core.hpp>
+#include <dv-processing/core/frame.hpp>
 #include <dv-processing/io/mono_camera_recording.hpp>
-using std::cout, std::endl;
+#include <dv-processing/core/frame.hpp>
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+
+// TODO: Probably just using namespace std is fine
+using std::cout, std::cerr, std::endl;
+using std::vector, std::string, std::make_shared, std::shared_ptr, std::pair, std::array, std::tuple;
+using std::stoi, std::stoul, std::min, std::max, std::numeric_limits, std::abs;
+
+// We can pass in a user pointer to callback functions - shouldn't require an updater; vars have inf lifespan
+// GLFWwindow *g_window;
+bool g_keyToggles[256] = {false};
 
 int main() {
-    cout << "Keep on rocking the free world!" << endl;
-
+    cout << "Go package resolution https://davidmcelroy.org/?p=9964!!" << endl;
     return 0;
 }
